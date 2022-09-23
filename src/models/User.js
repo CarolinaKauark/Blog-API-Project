6d095b039687ebd1da8fa4ad349a6678232766d8
Ckauark <ckauark@gmail.com>
2022-09-23 16:03:27 -0300

    fix: test github

---
+++ src/models/User.js
@@ -0,0 +1,10 @@
+const { DataTypes } = require("sequelize/types");
+
+const UserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  })
+}
