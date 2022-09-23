e66817eae643cb649808db8f4fe50cdba8dafdc2
Ckauark <ckauark@gmail.com>
2022-09-23 16:21:02 -0300

    feat: model User

--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,25 @@
-const { DataTypes } = require("sequelize/types");
-
-const UserModel = (sequelize, DataTypes) => {
+module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true, 
+    },
     displayName: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     image: DataTypes.STRING,
-  })
+  }, 
+  {
+    timestamps: false,
+    underscored: true,
+    tableName: 'users',
+  });
+
+  // User.associate = (models) => {
+  //   User.hasOne(models.BlogPost, {
+  //     foreignKey: 'user_id', as: 'user'
+  //   });
+  // }
+
+  return User;
 }
