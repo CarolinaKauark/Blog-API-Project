c1074dc251919c97e908d7f4dff911414ec95cb9
Ckauark <ckauark@gmail.com>
2022-09-26 15:57:12 -0300

    refactor: login service

--- src/services/login.service.js
+++ src/services/login.service.js
@@ -2,8 +2,8 @@ const { User } = require('../models');
 const errorGenerate = require('../utils/errorGenerate');
 
 const login = async (email, password) => {
-  const user = await User.findOne({ where: { email } });
-  if (!user || user.password !== password) throw errorGenerate(400, 'Invalid fields');
+  const user = await User.findOne({ where: { email, password } });
+  if (!user) throw errorGenerate(400, 'Invalid fields');
 
   return user;
 };
